08f1975d14a1af240a248b89c2b5173a94cdb18a
lapixo-tech <alatini@gmail.com>
2021-08-16 09:57:40 -0300

    actualizamos scripts

--- blink-led-3times.py
+++ blink-led-3times.py
@@ -1,9 +1,13 @@
-#apagar led en posición 18
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
+for x in range(0,3):
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(18,GPIO.LOW)
+    time.sleep(0.5)
+       
 GPIO.output(18,GPIO.HIGH)
-
--- turn-off-led.py
+++ turn-off-led.py
@@ -5,5 +5,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(18,GPIO.LOW)
 
